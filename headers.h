359956d437168fca0650664ebacb75f43d01084e
qtgeo1248 <gzhou00@stuy.edu>
2019-10-29 15:19:37 -0400

    fsldjfls

--- headers.h
+++
@@ -1,8 +0,0 @@
-struct song_node{
-  char name[100];
-  char artist[100];
-  struct song_node *next;
-};
-
-void print_list(struct song_node * n);
-struct song_node * free_list(struct song_node * n);
