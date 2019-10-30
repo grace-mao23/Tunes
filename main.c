0f6e6749cd462516a8868155149d134853c00c51
qtgeo1248 <gzhou00@stuy.edu>
2019-10-30 14:47:15 -0400

    started main

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "tunes.h"
 
 int main() {
     srand(time(NULL));
-
-
+    printf("%s\n\n", "TESTING LINKED LIST");
 
   return 0;
 }
