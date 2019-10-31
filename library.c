82552bfef4d438616ab1a1f9133122c32d71016a
qtgeo1248 <gzhou00@stuy.edu>
2019-10-31 15:29:44 -0400

    fixed grace's incorrect tabbing

--- library.c
+++ library.c
@@ -5,35 +5,35 @@
 
 // helper function to turn character into number 0 to 27
 int letterChar(char c) {
-  if (c >= 97 || c <= 122) {
-    return ((int)c) % 97;
-  } else {
-    return 26;
-  }
+    if (c >= 97 || c <= 122) {
+        return ((int)c) % 97;
+    } else {
+        return 26;
+    }
 }
 
 // add song nodes
 void add_song(struct song_node *table[27], char *name, char *artist) {
-  int place = letterChar(artist[0]);
-  table[place] = insert_sort(table[place], name, artist);
+    int place = letterChar(artist[0]);
+    table[place] = insert_sort(table[place], name, artist);
 }
 
 // search for a song given song and artist name
 struct song_node * search_song(struct song_node *table[27], char *name, char *artist) {
-  int letter = letterChar(artist[0]);
-  return find_song(table[letter], name, artist);
+    int letter = letterChar(artist[0]);
+    return find_song(table[letter], name, artist);
 }
 
 // search for an artist
 struct song_node * search_artist(struct song_node *table[27], char *artist) {
-  int letter = letterChar(artist[0]);
-  return find_artist(table[letter], artist);
+    int letter = letterChar(artist[0]);
+    return find_artist(table[letter], artist);
 }
 
 // print out all entries under a certain letter
 void print_letter(struct song_node *table[27], char letter) {
-  int i = letterChar(letter);
-  print_list(table[i]);
+    int i = letterChar(letter);
+    print_list(table[i]);
 }
 
 // print out all the songs of a certain artist
@@ -41,10 +41,10 @@ void print_letter(struct song_node *table[27], char letter) {
 
 // print out entire library
 void print_library(struct song_node *table[27]) {
-  for (int i = 0; i < 27; i++) {
-    print_list(table[i]);
-    printf("\n");
-  }
+    for (int i = 0; i < 27; i++) {
+        print_list(table[i]);
+        printf("\n");
+    }
 }
 
 // shuffle
