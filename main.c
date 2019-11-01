9fa98c68dbf82142dfbcc71f016d4e092799e831
qtgeo1248 <gzhou00@stuy.edu>
2019-11-01 15:14:33 -0400

    added more tests

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
 
 int main() {
     srand(time(NULL));
-    printf("\n\nTESTING INSERT FRONT AND PRINT NODE\n\n");
+
+    printf("\n\nLINKED LIST TESTS\n\n\n");
+    printf("\nTesting Insert Front and Print List\n\n");
     struct song_node *head = NULL;
     head = insert_front(head, "black magic", "little mix");
     printf("Inserting \"black magic\" by little mix\n");
@@ -18,7 +20,7 @@ int main() {
     printf("Inserting \"i kissed a girl\" by katy perry\n");
     print_list(head);
     head = insert_front(head, "furioso melodia", "gmtn");
-    printf("Inserting \"furioso melodia\" gmtn\n");
+    printf("Inserting \"furioso melodia\" by gmtn\n");
     print_list(head);
     head = insert_front(head, "the a team", "ed sheeran");
     printf("Inserting \"the a team\" by ed sheeran\n");
@@ -28,19 +30,29 @@ int main() {
     print_list(head);
 
 
-    printf("\n\nTESTING INSERT SORT\n\n");
+    printf("\nTesting Insert Sort\n\n");
     head = insert_sort(head, "just give me a reason", "p!nk");
     printf("Inserting \"just give me a reason\" by p!nk\n");
     print_list(head);
     head = insert_sort(head, "despacito", "luis fonsi");
     printf("Inserting \"despacito\" by luis fonsi\n");
     print_list(head);
-    head = insert_sort(head, "despacito", "luis fonsi");
-    printf("Inserting \"despacito\" by luis fonsi\n");
+    head = insert_sort(head, "lights", "ellie goulding");
+    printf("Inserting \"lights\" by ellie goulding\n");
+    print_list(head);
+    head = insert_sort(head, "rap god", "eminem");
+    printf("Inserting \"rap god\" by eminem\n");
     print_list(head);
     head = insert_sort(head, "santa tell me", "ariana grande");
     printf("Inserting \"santa tell me\" by ariana grande\n");
     print_list(head);
 
+    printf("\nTesting Find Song\n\n");
+    printf("Looking for [gtmn: furioso melodia]\n");
+    print_list(find_song(head, "furioso melodia", "gmtn"));
+    printf("Looking for [p!nk: just give me a reason]\n");
+    print_list(find_song(head, "just give me a reason", "p!nk"));
+    printf("Looking for [little mix: black magic]\n");
+    print_list(find_song(head, "black magic", "little mix"));
   return 0;
 }
