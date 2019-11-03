fafe32bfef24ebb258f93cc539b32ee721036b58
qtgeo1248 <gzhou00@stuy.edu>
2019-11-03 15:20:22 -0500

    finsihed random

--- library.c
+++ library.c
@@ -71,7 +71,31 @@ void print_artist(struct song_node *table[27], char *author) {
 }
 
 // shuffle
-
+void print_random(struct song_node *table[27]) {
+    struct song_node *entirety = NULL;
+    int i = 0;
+    for (; i < 27; i++) {
+        if (table[i] != NULL) {
+            struct song_node *first = table[i];
+            struct song_node *second = first->next;
+            while (second != NULL) {
+                entirety = insert_front(entirety, first->title, first->artist);
+                first = second;
+                second = second->next;
+            }
+            entirety = insert_front(entirety, first->title, first->artist);
+        }
+    }
+    if (size(entirety) == 0) {
+        printf("no songs in library\n");
+    } else {
+        int number = (rand() % (size(entirety) - 1)) + 1;
+        for (; number > 0; number--) {
+            struct song_node *current = find_random(entirety);
+            printf("\"%s\" by %s\n", current->title, current->artist);
+        }
+    }
+}
 
 // delete a song
 
