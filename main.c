6e373deda06868650fe05d76b95c70b1f422af23
qtgeo1248 <gzhou00@stuy.edu>
2019-11-03 01:37:30 -0500

    tested compare list

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main() {
     srand(time(NULL));
 
     printf("\n\nLINKED LIST TESTS\n\n\n");
-    printf("\nTesting Insert Front and Print List\n\n");
+    printf("Testing Insert Front and Print List\n\n");
     struct song_node *head = NULL;
     printf("–––Inserting \"black magic\" by little mix\n");
     head = insert_front(head, "black magic", "little mix");
@@ -28,6 +28,49 @@ int main() {
     printf("–––Inserting \"beautiful people\" by ed sheeran\n");
     head = insert_front(head, "beautiful people", "ed sheeran");
     print_list(head);
+
+    printf("\nTesting Songcmp Full (inputs are strings)\n\n");
+    struct song_node *compare = NULL;
+    printf("Comparing [gmtn: furioso melodia] = [gmtn: furioso melodia]\n");
+    compare = insert_front(compare, "furioso melodia", "gmtn");
+    printf("%d\n", songcmp_full(compare, "furioso melodia", "gmtn"));
+    printf("Comparing [ed sheeran: the a team] > [ariana grande: one last time]\n");
+    compare = insert_front(compare, "the a team", "ed sheeran");
+    printf("%d\n", songcmp_full(compare, "one last time", "ariana grande"));
+    printf("Comparing [p!nk: what about us] < [zedd: clarity]\n");
+    compare = insert_front(compare, "what about us", "p!nk");
+    printf("%d\n", songcmp_full(compare, "clarity", "zedd"));
+    printf("Comparing [ariana grande: breathin] < [ariana grande: one last time]\n");
+    compare = insert_front(compare, "breathin", "ariana grande");
+    printf("%d\n", songcmp_full(compare, "one last time", "ariana grande"));
+    printf("Comparing [ariana grande: santa tell me] > [ariana grande: break up with your girlfriend]\n");
+    compare = insert_front(compare, "santa tell me", "ariana grande");
+    printf("%d\n", songcmp_full(compare, "break up with your girlfriend", "ariana grande"));
+
+
+    printf("\nTesting Songcmp Full (inputs are pointers)\n\n");
+    struct song_node *compare1 = NULL;
+    struct song_node *compare2 = NULL;
+    printf("Comparing [gmtn: furioso melodia] = [gmtn: furioso melodia]\n");
+    compare1 = insert_front(compare1, "furioso melodia", "gmtn");
+    compare2 = insert_front(compare2, "furioso melodia", "gmtn");
+    printf("%d\n", songcmp_short(compare1, compare2));
+    printf("Comparing [ed sheeran: the a team] > [ariana grande: one last time]\n");
+    compare1 = insert_front(compare1, "the a team", "ed sheeran");
+    compare2 = insert_front(compare2, "one last time", "ariana grande");
+    printf("%d\n", songcmp_short(compare1, compare2));
+    printf("Comparing [p!nk: what about us] < [zedd: clarity]\n");
+    compare1 = insert_front(compare1, "what about us", "p!nk");
+    compare2 = insert_front(compare2, "clarity", "zedd");
+    printf("%d\n", songcmp_short(compare1, compare2));
+    printf("Comparing [ariana grande: breathin] < [ariana grande: one last time]\n");
+    compare1 = insert_front(compare1, "breathin", "ariana grande");
+    compare2 = insert_front(compare2, "one last time", "ariana grande");
+    printf("%d\n", songcmp_short(compare1, compare2));
+    printf("Comparing [ariana grande: santa tell me] > [ariana grande: break up with your girlfriend]\n");
+    compare1 = insert_front(compare1, "santa tell me", "ariana grande");
+    compare2 = insert_front(compare2, "break up with your boyfriend", "ariana grande");
+    printf("%d\n", songcmp_short(compare1, compare2));
 
     printf("\nTesting Insert Sort\n\n");
     printf("–––Inserting \"just give me a reason\" by p!nk\n");
